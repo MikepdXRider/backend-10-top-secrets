fc8d935eb59023e2cf82791e43b7e561d2c31a7b
Michael Rider <mikepdxrider@gmail.com>
2022-02-08 09:12:47 -0800

    adds initial app archetecture and comments

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,13 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+-- Notes table
+-- note_id primary
+-- user_id foreign
+-- note_text
+
+
+-- Users table
+-- user_id primary
+-- hash_password
+-- user_email
