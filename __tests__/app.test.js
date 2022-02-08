e827cc18e7e690d0eb992695a121137e70703540
Michael Rider <mikepdxrider@gmail.com>
2022-02-08 10:48:40 -0800

    adds failing post user test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,11 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+const mockUser = {
+  email: 'test-email@test.com',
+  password: 'test-password',
+};
+
 describe('backend-10-top-secrets routes', () => {
   beforeEach(() => {
     return setup(pool);
@@ -10,5 +15,12 @@ describe('backend-10-top-secrets routes', () => {
 
   afterAll(() => {
     pool.end();
+  });
+
+  it('posts a user and recieves a user object in the correct shape', async () => {
+    const res = await request(app).post('/api/v1/users').send(mockUser);
+    const actual = res.body;
+    const expected = { email: mockUser.email, id: expect.any(Number) };
+    expect(actual).toEqual(expected);
   });
 });
