88b41ec483feb20ee87d35ff847db12dfccf84cd
Michael Rider <mikepdxrider@gmail.com>
2022-02-08 16:38:00 -0800

    adds failing login user test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -17,9 +17,19 @@ describe('backend-10-top-secrets routes', () => {
     pool.end();
   });
 
-  it('posts a user and recieves a user object in the correct shape', async () => {
+  it('attempts to create a new user and recieves a user object in the correct shape', async () => {
     const res = await request(app).post('/api/v1/users/').send(mockUser);
     const actual = res.body;
+    const expected = { email: mockUser.email, id: expect.any(String) };
+    expect(actual).toEqual(expected);
+  });
+
+  it('attempts to login an existing user and recieves a user object in the correct shape', async () => {
+    await request(app).post('/api/v1/users/').send(mockUser);
+    const loginUserRes = await request(app)
+      .post('/api/v1/users/session')
+      .send(mockUser);
+    const actual = loginUserRes.body;
     const expected = { email: mockUser.email, id: expect.any(String) };
     expect(actual).toEqual(expected);
   });
